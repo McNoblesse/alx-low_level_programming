c20429e2987534b4c055518ffda4128422660886
McNoblesse <noblessejosh@gmail.com>
2022-09-09 00:44:23 +0100

    A C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	/*Initialize String*/
+	char MyStr[] = "Programming is like building a multilingual puzzle";
+
+	puts(MyStr); 	/* Writing the valie of MyStr to stdout*/
+	
+	return (0);
+}
+
