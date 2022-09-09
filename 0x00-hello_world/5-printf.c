847504f7736bd37355c76c64afdf6f2a024a1494
Noblesse <nobleindepth@gmail.com>
2022-09-09 17:45:47 +0800

    Update on 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * Initialize and 
- * print formated stdout
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
