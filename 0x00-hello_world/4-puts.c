bbeda88e32a1c734599c10ff0eed828a0e9f7b45
McNoblesse <noblessejosh@gmail.com>
2022-09-09 01:10:59 +0100

    Updated 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	/*Initialize String*/
 	char MyStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(MyStr); /* Writing the valie of MyStr to stdout*/
-	
 	return (0);
 }
-
