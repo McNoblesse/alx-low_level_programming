16e6deb1d6ed8f93a1f00a3f1aed363c12b219eb
McNoblesse <noblessejosh@gmail.com>
2022-09-11 15:49:55 +0100

    A program that prints the alphabet in lowercase, followed by a new line.
    Print all the letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print the Alphabets in lowercase
+ * and exclude q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char low_alpha;
+
+	char e = 'e'
+
+	char q = 'q'
+
+        for (low_alpha = 'a'; low_alpha <= 'z'; low_alpha++)
+        {
+		for (low_alpha != 'e' && low_alpha != 'q')
+			putchar(low_alpha);
+        }
+        putchar('\n');
+
+        return (0);
+}
